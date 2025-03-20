0ba50601fb201ec230148d2d476e8e8cd51db803
lu_chan <lou.97213@gmail.com>
2025-03-20 05:20:53 +0000

    add history

--- src/web-search.py
+++ src/web-search.py
@@ -1,9 +1,11 @@
 import json
 import requests
 import pandas as pd
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from concurrent.futures import ThreadPoolExecutor
 import numpy as np
 import os
+import logging
+import time
 
 # get conf
 import configparser
@@ -19,13 +21,12 @@ from openai import OpenAI
 
 client = OpenAI(api_key=openai_api_key)
 
-from typing import Annotated
-from typing_extensions import TypedDict
+from typing_extensions import TypedDict, Annotated
 from langgraph.graph import StateGraph, START, END
 from langchain_core.documents import Document
 from langchain_community.document_loaders import WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from dataclasses import dataclass
 
 
@@ -71,8 +72,14 @@ class TokenLengthCalculator:
 model_name = "gpt-4o"
 
 
+def add_messages_to_history(new: list, old: list):
+    new.extend(old)
+    return new
+
+
 class State(TypedDict):
-    user_input: str
+    current_user_input: str
+    history: Annotated[list, add_messages_to_history]
     need_web_search: bool
     keyword: str
     urls: list[str]
@@ -85,37 +92,66 @@ class State(TypedDict):
     manual_web_search_answer: str
 
 
-def log_node_execution(node_name):
+logging.basicConfig(level=logging.INFO)
+
+def log_node_execution(node_name, print_state=False, state_keys_to_print=None):
     def decorator(func):
         def wrapper(state, *args, **kwargs):
-            print(f"Executing node: {node_name}")
-            # print(f"State before execution: {state}")
+            start_time = time.time()
+            logging.info(f"Executing node: {node_name}")
+            if print_state:
+                if state_keys_to_print:
+                    filtered_state = {
+                        key: state[key] for key in state_keys_to_print if key in state
+                    }
+                    logging.debug(f"Filtered state: {filtered_state}")
+                else:
+                    logging.debug(f"State before execution: {state}")
             result = func(state, *args, **kwargs)
+            end_time = time.time()
+            logging.info(f"Node {node_name} executed in {end_time - start_time:.2f}s")
             return result
-
         return wrapper
+    return decorator
 
+def log_node_execution_async(node_name, print_state=False, state_keys_to_print=None):
+    def decorator(func):
+        async def wrapper(state, *args, **kwargs):
+            start_time = time.time()
+            logging.info(f"Executing node: {node_name}")
+            if print_state:
+                if state_keys_to_print:
+                    filtered_state = {
+                        key: state[key] for key in state_keys_to_print if key in state
+                    }
+                    logging.debug(f"Filtered state: {filtered_state}")
+                else:
+                    logging.debug(f"State before execution: {state}")
+            result = await func(state, *args, **kwargs)
+            end_time = time.time()
+            logging.info(f"Node {node_name} executed in {end_time - start_time:.2f}s")
+            return result
+        return wrapper
     return decorator
 
 
 @log_node_execution("get_user_input")
 def get_user_input(state: State):
     user_input = input("User: ")
+    return {"current_user_input": user_input}
+
+
+@log_node_execution("check_use_web_search")
+def check_use_web_search(state: State):
     need_web_search = input("Do you want to search the web? (y/n): ") == "y"
-    return {"user_input": user_input, "need_web_search": need_web_search}
-
-
-@log_node_execution("need_web_search")
-def need_web_search(state: State) -> str:
-    if state["need_web_search"]:
-        return "Y"
-    else:
-        return "N"
+    return {"need_web_search": need_web_search}
 
 
 @log_node_execution("extract_web_search_keyword")
 def extract_web_search_keyword(state: State):
-    prompt = f'Extract the search query from the user input: "{state["user_input"]}"'
+    prompt = (
+        f'Extract the search query from the user input: "{state["current_user_input"]}"'
+    )
     response = client.responses.create(
         model="gpt-4o",
         input=[
@@ -145,7 +181,7 @@ def extract_web_search_keyword(state: State):
     return {"keyword": json.loads(response.output[0].arguments).get("keyword")}
 
 
-@log_node_execution("google_search")
+@log_node_execution("google_search", True, ["keyword"])
 def google_search(state: State):
     google_api_key = config["google"]["api_key"]
     cse_id = config["google"]["cse_id"]
@@ -156,17 +192,38 @@ def google_search(state: State):
     return {"urls": [web.get("link") for web in web_pages.json().get("items", [])]}
 
 
+# @log_node_execution_async("fetch_web_pages_to_documents", True)
+# async def playwright_fetch_web_pages_to_documents(state: State):
+#     from langchain_community.tools.playwright.utils import (
+#         create_async_playwright_browser,
+#     )
+#     from langchain_community.agent_toolkits import PlayWrightBrowserToolkit
+#     import nest_asyncio
+#     nest_asyncio.apply()
+#     async_browser = create_async_playwright_browser()
+#     toolkit = PlayWrightBrowserToolkit.from_browser(async_browser=async_browser)
+#     tools = toolkit.get_tools()
+#     tools_by_name = {tool.name: tool for tool in tools}
+#     navigate_tool = tools_by_name["navigate_browser"]
+#     extract_text_tool = tools_by_name["extract_text"]
+#     extracted_texts = []
+#     for url in state["urls"]:
+#         await navigate_tool.arun({"url": url})
+#         page_content = await extract_text_tool.arun({})
+#         extracted_texts.append(page_content)
+
+
 @log_node_execution("fetch_web_pages_to_documents")
 def fetch_web_pages_to_documents(state: State):
     loader = WebBaseLoader(web_path=state["urls"])
     web_documents = loader.load()
-    
+
     for i, doc in enumerate(web_documents):
         file_path = f"/webSearch/ref{i + 1}.txt"
         with open(file_path, "w", encoding="utf-8") as f:
             f.write(doc.page_content)
         print(f"Document saved to {file_path}")
-    
+
     return {"web_documents": web_documents}
 
 
@@ -188,96 +245,6 @@ def chunking_documents(state: State):
     return {"chunked_documents": chunked_documents}
 
 
-# @log_node_execution("cluster_documents")
-# def cluster_documents(documents, cluster_size):
-#     def num_tokens_from_string(string: str) -> int:
-#         """Returns the number of tokens in a text string."""
-#         encoding = tiktoken.get_encoding("cl100k_base")
-#         num_tokens = len(encoding.encode(string))
-#         return num_tokens
-
-#     embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
-
-#     def process_embedding(content):
-#         used_token = num_tokens_from_string(content)
-#         return embeddings.embed_documents([content])[0], used_token
-
-#     with ThreadPoolExecutor(max_workers=5) as executor:
-#         futures = {
-#             executor.submit(process_embedding, doc.page_content): i
-#             for i, doc in enumerate(documents)
-#         }
-#         vectors = []
-
-#         for future in as_completed(futures):
-#             try:
-#                 result, used_token = future.result()
-#                 vectors.append(result)
-#             except Exception as e:
-#                 print(f"Error embedding document index {futures[future]}: {e}")
-
-#     vectors = np.array(vectors)
-#     kmeans = KMeans(n_clusters=cluster_size, random_state=42).fit(vectors)
-
-#     closest_indices = []
-#     for i in range(cluster_size):
-#         distances = np.linalg.norm(vectors - kmeans.cluster_centers_[i], axis=1)
-#         closest_index = np.argmin(distances)
-#         closest_indices.append(closest_index)
-
-#     selected_indices = sorted(closest_indices)
-#     new_docs = [documents[index] for index in selected_indices]
-#     return {"chunked_documents": new_docs}
-
-
-# @log_node_execution("need_cluster")
-# def need_cluster(state: State) -> str:
-#     if len(state["chunked_documents"]) > math.floor(
-#         model_info[model_name].max_input / model_info[model_name].max_output
-#     ):
-#         return "Y"
-#     return "N"
-
-
-# @log_node_execution("calc_documents_token")
-# def calc_documents_token(state: State):
-#     token_calculator = TokenLengthCalculator(model_name)
-#     total_tokens = 0
-#     for doc in state["chunked_documents"]:
-#         total_tokens += token_calculator.tiktoken_len(doc)
-#     return {"total_tokens": total_tokens}
-
-
-# @log_node_execution("need_map")
-# def need_map(state: State) -> str:
-#     if state["total_tokens"] > model_info[model_name].max_input:
-#         return "Y"
-#     return "N"
-
-# @log_node_execution("need_reduce")
-# def need_reduce(state: State) -> str:
-#     if state["total_tokens"] > model_info[model_name].max_input:
-#         return "Y"
-#     return "N"
-
-
-# @log_node_execution("map_documents")
-# def map_documents(state: State):
-#     map_documents = []
-#     for doc in state["chunked_documents"]:
-#         stripped_document = doc.replace("\n", " ").strip()
-#         document_summarize = ask_llm(stripped_document, "請為以下的文本生成摘要")
-#         map_documents.append(document_summarize)
-#     breakpoint()
-#     return {"chunked_documents": map_documents}
-
-# @log_node_execution("reduce_documents")
-# def reduce_documents(state: State):
-#     reduced_context = ask_llm(state["context"], "請為以下的文本生成摘要")
-#     breakpoint()
-#     return {"context": reduced_context}
-
-
 @log_node_execution("retrieval_documents")
 def retrieval_documents(state: State):
     def cosine_similarity(vec1, vec2):
@@ -285,30 +252,32 @@ def retrieval_documents(state: State):
         norm_vec1 = np.linalg.norm(vec1)
         norm_vec2 = np.linalg.norm(vec2)
         return dot_product / (norm_vec1 * norm_vec2)
+
     embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
     chunk_embeddings = embeddings.embed_documents(state["chunked_documents"])
-    
+
     embeddings_df = pd.DataFrame(chunk_embeddings)
     embeddings_csv_path = "/webSearch/embeddings.csv"
     embeddings_df.to_csv(embeddings_csv_path, index=False)
     print(f"Embeddings saved to {embeddings_csv_path}")
-    
-    user_input_embedding = embeddings.embed_query(state["user_input"])
-    
+
+    user_input_embedding = embeddings.embed_query(state["current_user_input"])
+
     def calculate_similarity(index):
         return index, cosine_similarity(user_input_embedding, chunk_embeddings[index])
-    
+
     with ThreadPoolExecutor() as executor:
-        similarities = list(executor.map(calculate_similarity, range(len(chunk_embeddings))))
-    
+        similarities = list(
+            executor.map(calculate_similarity, range(len(chunk_embeddings)))
+        )
+
     similarities.sort(key=lambda x: x[1], reverse=True)
     top_indices = [index for index, _ in similarities[:5]]
     top_chunks = [state["chunked_documents"][i] for i in top_indices]
-    breakpoint()
     return {"context": top_chunks}
 
 
-@log_node_execution("combine_documents")
+@log_node_execution("combine_documents", True, ["context"])
 def combine_documents(state: State):
     combined_text = ""
     for doc in state["chunked_documents"]:
@@ -318,26 +287,37 @@ def combine_documents(state: State):
 
 @log_node_execution("generate_ai_message")
 def generate_ai_message(state: State):
-    response = ask_llm(state["user_input"], state["context"])
+    response = ask_llm(
+        state["current_user_input"], state["context"], None, None, state["history"]
+    )
     return {"manual_web_search_answer": response.output_text}
 
 
 @log_node_execution("chatbot")
 def chatbot(state: State):
-    response = ask_llm(state["user_input"], "")
+    response = ask_llm(state["current_user_input"], "", None, None, state["history"])
     return {"display_message": response.output_text}
 
 
 @log_node_execution("web_search_by_openai_built_in_tool")
 def web_search_by_openai_built_in_tool(state: State):
     response = ask_llm(
-        state["user_input"], "", [{"type": "web_search_preview"}], "required"
+        state["current_user_input"],
+        "",
+        [{"type": "web_search_preview"}],
+        "required",
+        state["history"],
     )
     return {"openai_responses_api_answer": response.output_text}
 
 
-def ask_llm(question, context, tools=None, tool_choice=None):
+def ask_llm(question, context, tools=None, tool_choice=None, history=[]):
     messages = []
+    messages.append(
+        {"role": "developer", "content": "使用者國籍為台灣, 請嚴格執行輸出只用繁體中文"}
+    )
+    if len(history) > 0:
+        messages.extend(history)
     if len(context) > 0:
         messages.append({"role": "developer", "content": context})
     if len(question) > 0:
@@ -375,23 +355,41 @@ def print_answer(state: State):
             f'openai_responses_resp.output_text: {state["openai_responses_api_answer"]}'
         )
         print("---------------------------------------------------------------")
-        return
+        return {
+            "history": [
+                {"role": "user", "content": state["current_user_input"]},
+                {"role": "assistant", "content": state["openai_responses_api_answer"]},
+            ]
+        }
     print("---------------------------------------------------------------")
     print(f'chatbot_resp.output_text: {state["display_message"]}')
     print("---------------------------------------------------------------")
 
 
+@log_node_execution("need_web_search")
+def need_web_search(state: State) -> str:
+    if state["need_web_search"]:
+        return "Y"
+    else:
+        return "N"
+
+
+@log_node_execution("should_quit")
+def should_quit(state: State) -> str:
+    quit_keywords = {"q", "quit", "exit"}
+    if state["current_user_input"].strip().lower() in quit_keywords:
+        return "Y"
+    return "N"
+
+
 graph_builder = StateGraph(State)
 graph_builder.add_node("get_user_input", get_user_input)
+graph_builder.add_node("check_use_web_search", check_use_web_search)
 graph_builder.add_node("chatbot", chatbot)
 graph_builder.add_node("extract_web_search_keyword", extract_web_search_keyword)
 graph_builder.add_node("google_search", google_search)
 graph_builder.add_node("fetch_web_pages_to_documents", fetch_web_pages_to_documents)
 graph_builder.add_node("chunking_documents", chunking_documents)
-# graph_builder.add_node("cluster_documents", cluster_documents)
-# graph_builder.add_node("calc_documents_token", calc_documents_token)
-# graph_builder.add_node("map_documents", map_documents)
-# graph_builder.add_node("reduce_documents", reduce_documents)
 graph_builder.add_node("retrieval_documents", retrieval_documents)
 graph_builder.add_node("combine_documents", combine_documents)
 graph_builder.add_node("generate_ai_message", generate_ai_message)
@@ -404,6 +402,11 @@ graph_builder.add_edge(START, "gen_graph_png")
 graph_builder.add_edge("gen_graph_png", "get_user_input")
 graph_builder.add_conditional_edges(
     "get_user_input",
+    should_quit,
+    {"Y": END, "N": "check_use_web_search"},
+)
+graph_builder.add_conditional_edges(
+    "check_use_web_search",
     need_web_search,
     {"Y": "extract_web_search_keyword", "N": "chatbot"},
 )
@@ -417,8 +420,17 @@ graph_builder.add_edge("chunking_documents", "retrieval_documents")
 graph_builder.add_edge("retrieval_documents", "combine_documents")
 graph_builder.add_edge("combine_documents", "generate_ai_message")
 graph_builder.add_edge("chatbot", "print_answer")
+graph_builder.add_edge("print_answer", "get_user_input")
 graph_builder.add_edge(
     ["generate_ai_message", "web_search_by_openai_built_in_tool"], "print_answer"
 )
 graph = graph_builder.compile()
-graph.invoke(input={})
+
+
+async def main():
+    await graph.ainvoke(input={})
+
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
